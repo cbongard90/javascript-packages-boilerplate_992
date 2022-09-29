46c0fd65c897a74c619f628f3f4b7e3ccc25d9d6
Christian Bongard <christian.bongard90@gmail.com>
2022-09-29 09:43:35 +0400

    added a reset link for the button

--- src/controllers/disable_button_controller.js
+++ src/controllers/disable_button_controller.js
@@ -1,13 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["button", "link"]
 
   connect() {
     console.log("Hello from our first Stimulus controller")
   }
 
   disable() {
-    this.element.innerText = "Bingo!"
-    this.element.setAttribute("disabled", "")
+    // this.element.innerText = "Bingo!"
+    // this.element.setAttribute("disabled", "")
+
+    this.buttonTarget.innerText = "Bingo!"
+    this.buttonTarget.setAttribute("disabled", "")
+    this.linkTarget.classList.remove("d-none")
   }
 }
