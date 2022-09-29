faf36be1f67c03aa961ad030f5971da59c927c5a
Christian Bongard <christian.bongard90@gmail.com>
2022-09-29 09:46:56 +0400

    added the reset function for the reset link

--- src/controllers/disable_button_controller.js
+++ src/controllers/disable_button_controller.js
@@ -15,4 +15,10 @@ export default class extends Controller {
     this.buttonTarget.setAttribute("disabled", "")
     this.linkTarget.classList.remove("d-none")
   }
+
+  reset() {
+    this.buttonTarget.innerText = "Click me!"
+    this.buttonTarget.removeAttribute("disabled")
+    this.linkTarget.classList.add("d-none")
+  }
 }
